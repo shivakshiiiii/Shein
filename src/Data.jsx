8bffc4a8a46bfa0dd32696d572629e5117ed4129
shivakshi <shivakshi0660.be20@chitkara.edu.in>
2022-10-26 16:12:29 +0530

    Add files via upload

---
+++ src/Data.jsx
@@ -0,0 +1,46 @@
+const DATA = [
+    {
+        id : 0,
+        title : "Rhinstone Silver Ring",
+        price : 999,
+        desc : "Metal ring decorated with shiny glass stones.",
+        img : "/assets/images/products/1.jpg"
+    },
+    {
+        id : 1,
+        title : "Beige Wide Cullotes",
+        price : 1999,
+        desc : "Trousers in an airy crêpe weave with pleats at the front. High waist with covered elastication at the back, a concealed zip in one side and long, wide legs..",
+        img : "/assets/images/products/2.jpg"
+    },
+    {
+        id : 2,
+        title : "Red Crop Blouse",
+        price : 1299,
+        desc : "Cropped top in soft jersey with narrow shoulder straps.",
+        img : "/assets/images/products/3.jpg"
+    },
+    {
+        id : 3,
+        title : "Black Heeled Boots",
+        price : 3390,
+        desc : "Ankle boots with covered block heels and a concealed zip in one side. Jersey linings. Heel 8.5 cm",
+        img : "/assets/images/products/4.jpg"
+    },
+    {
+        id : 4,
+        title : "Puffer Jacket",
+        price : 2290,
+        desc : "Puffer jacket with lined black strip and 19.8cm in lenght",
+        img : "/assets/images/products/5.jpg"
+    },
+    {
+        id : 5,
+        title : "One Shoulder Top",
+        price : 990,
+        desc : "crop one shoulder top in black color.looks elegant on hourglass body type",
+        img : "/assets/images/products/6.jpg"
+    }
+]
+
+export default DATA;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return (
+        <div>
+            <div  class="mt-5" style={{backgroundColor:'grey',color:'white',height:'120px',fontSize:'25px',textAlign:'center'}}> 
+            <p>
+            © 2022 Copyright by SHEIN. All Rights Reserved.
+            </p>
+            <p>-Shivakshi</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
